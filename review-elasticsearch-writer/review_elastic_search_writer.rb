732e66a3b3ea4e67f6da3851998cd565743d8d90
David Henry <david.henry@reevoo.com>
2013-07-22 16:14:42 +0100

    Elasticsearch writer working

---
+++ review-elasticsearch-writer/review_elastic_search_writer.rb
@@ -0,0 +1,62 @@
+require 'amqp'
+require 'json'
+require 'tire'
+require 'torque'
+
+class ReviewElasticSearchWriter
+  TOPIC_NAME  = 'reviews'
+  QUEUE_NAME  = 'elasticsearch.review.writer'
+  ROUTING_KEY = 'review.*'
+
+  def process(review_identifier)
+    message = revieworld_data(class: :reviews, conditions: review_identifier, format: :torque) do |torque_data|
+      Torque.to('reviews', torque_data)
+    end
+  end
+
+  def exchange
+    @exchange ||= channel.topic(TOPIC_NAME, auto_delete: true)
+  end
+
+# extract..
+
+  attr_reader :channel
+
+  def self.run!
+    AMQP.start do
+      channel = AMQP::Channel.new
+      new(channel).run!
+    end
+  end
+
+  def initialize(channel)
+    @channel = channel
+  end
+
+  def run!
+    channel.queue(QUEUE_NAME) do |queue|
+      queue.bind(exchange, routing_key: ROUTING_KEY).subscribe do |review_identifier|
+        process JSON.parse(review_identifier)
+      end
+    end
+  rescue Interrupt => e
+    puts 'Bye bye!'
+    EM.next_tick { AMQP.exit }
+    exit(0)
+  end
+
+  def revieworld_data(query)
+    channel.queue("", :exclusive => true, :auto_delete => true) do |replies_queue|
+      replies_queue.subscribe do |metadata, payload|
+        yield(JSON.parse(payload))
+      end
+      channel.default_exchange.publish(
+        query.to_json,
+        :routing_key => "revieworld.data-request.#{query[:class]}",
+        :message_id  => Kernel.rand(10101010).to_s,
+        :reply_to    => replies_queue.name
+      )
+    end
+
+  end
+end
---
+++ review-elasticsearch-writer/review_elastic_search_writer_spec.rb
@@ -0,0 +1,45 @@
+require 'rspec'
+require 'evented-spec'
+require 'pry'
+require 'pry-nav'
+require_relative 'review_elastic_search_writer'
+
+describe 'It writes data to torque' do
+  include EventedSpec::AMQPSpec
+  let(:message) { {'message' => 'hash'} }
+
+  amqp_before do
+    # initializing amqp channel
+    @channel   = AMQP::Channel.new
+    # using default amqp exchange
+    @exchange = @channel.topic(ReviewElasticSearchWriter::TOPIC_NAME)
+  end
+
+  it 'will add record to elasticsearch' do
+    subsitute_revieworld_data({class: :reviews, format: :torque}, message)
+
+    ReviewElasticSearchWriter.new(@channel).run!
+    Torque.should_receive(:to).with('reviews', message)
+
+    @exchange.publish({id: 14}.to_json, :key => 'review.create')
+
+    done(0.2) {
+      # After #done is invoked, it launches an optional callback
+
+      # Here goes the main check
+    }
+  end
+
+  def subsitute_revieworld_data(matcher, response)
+    requests_queue = @channel.queue("revieworld.data-request.#{matcher[:class]}", :exclusive => true, :auto_delete => true)
+    # requests_queue = @channel.queue("revieworld.data-request", :exclusive => true, :auto_delete => true)
+    requests_queue.subscribe(:ack => true) do |metadata, payload|
+      @channel.default_exchange.publish(response.to_json,
+                                       :routing_key    => metadata.reply_to,
+                                       :correlation_id => metadata.message_id,
+                                       :mandatory      => true)
+
+      metadata.ack
+    end
+  end
+end
